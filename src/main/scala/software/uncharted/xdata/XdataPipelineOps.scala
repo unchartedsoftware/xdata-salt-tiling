00a480a98e72a77d17289a63a70c8089b6de03c1
Chris Bethune <cbethune@oculusinfo.com>
2015-12-08 00:12:55 -0500

    baseline project with build and skeleton src files

---
+++ src/main/scala/software/uncharted/xdata/XdataPipelineOps.scala
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2014-2015 Uncharted Software Inc. All rights reserved.
+ *
+ * Property of Uncharted(tm), formerly Oculus Info Inc.
+ * http://uncharted.software/
+ *
+ * This software is the confidential and proprietary information of
+ * Uncharted Software Inc. ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Uncharted Software Inc.
+ */
+package software.uncharted.xdata
+
+object XdataPipelineOps {
+  def foo(): Int = 0
+  def bar(): Int = -1
+}
