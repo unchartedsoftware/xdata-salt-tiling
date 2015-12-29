5b2386fb46b8fe9795fc243d9ff4419d235aaffa
Chris Bethune <cbethune@uncharted.software>
2015-12-29 00:47:24 -0500

    adds function to create spark sql schema from typesafe config

---
+++ src/main/scala/software/uncharted/xdata/sparkpipe/Schema.scala
@@ -0,0 +1,81 @@
+/**
+ * Copyright (c) 2014-2015 Uncharted Software Inc. All rights reserved.
+ *
+ * Property of Uncharted(tm), formerly Oculus Info Inc.
+ * http://uncharted.software/
+ *
+ * This software is the confidential and proprietary information of
+ * Uncharted Software Inc. ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Uncharted Software Inc.
+ */
+package software.uncharted.xdata.sparkpipe
+
+import com.typesafe.config.{Config, ConfigException}
+import grizzled.slf4j.Logging
+import scala.collection.JavaConverters.asScalaSetConverter
+import scala.collection.SortedMap
+import org.apache.spark.sql.types.{BooleanType, ByteType, DateType, DoubleType, FloatType, IntegerType,
+  LongType, ShortType, StringType, StructField, TimestampType, StructType}
+
+object Schema extends Logging {
+
+  case class FieldData(name: String, ftype: String, index: Int)
+
+  case class FieldDataException(message: String) extends Exception(message)
+
+  // scalastyle:off cyclomatic.complexity
+  // disable cyclomatic complexity because it gets tripped up by the legitimately
+  // large case statement
+  def apply(config: Config): Option[StructType] = {
+    try {
+      // Extract and sort fields
+      val fieldNames = config.getObject("fields").keySet().asScala
+      val fieldData = fieldNames.map { f =>
+        val fieldCfg = config.getConfig("fields").getConfig(f)
+        FieldData(f, fieldCfg.getString("type"), fieldCfg.getInt("index"))
+      }
+      val sortedFields = fieldData.toSeq.sortBy(_.index)
+
+      // Make sure there are no duplicate indices
+      val duplicateIndices = fieldData.groupBy(_.index)
+        .filter(p => p._2.size > 1)
+        .map(p => p._2.head.index)
+      if (duplicateIndices.nonEmpty) throw FieldDataException(s"Duplicate indices [$duplicateIndices]")
+
+      // Create a contiguous set of fields with stand-in data for unmapped columns
+      val fieldMap = sortedFields.map(s => s.index -> s)
+      val sortedMap = SortedMap(fieldMap:_*)
+      val contiguousFields = for (i <- 0 to sortedMap.lastKey) yield {
+        sortedMap.getOrElse(i, FieldData(s"__unspecified_${i}__", "string", i))
+      }
+
+      // Create the schema from the fields
+      val structFields = contiguousFields.map { fieldData =>
+        fieldData.ftype.toLowerCase match {
+          case "boolean" => StructField(fieldData.name, BooleanType)
+          case "byte" => StructField(fieldData.name, ByteType)
+          case "short" => StructField(fieldData.name, ShortType)
+          case "int" => StructField(fieldData.name, IntegerType)
+          case "long" => StructField(fieldData.name, LongType)
+          case "float" => StructField(fieldData.name, FloatType)
+          case "double" => StructField(fieldData.name, DoubleType)
+          case "string" => StructField(fieldData.name, StringType)
+          case "date" => StructField(fieldData.name, DateType)
+          case "timestamp" => StructField(fieldData.name, TimestampType)
+          case _ => throw FieldDataException(s"Unhandled variable type ${fieldData.ftype}")
+        }
+      }
+      Some(StructType(structFields))
+    } catch {
+      case e: ConfigException =>
+        error("Parse failure reading config file", e)
+        None
+      case fde: FieldDataException =>
+        error("Parse failure creating schema from config file", fde)
+        None
+    }
+  }
+  // scalastyle:on cyclomatic.complexity
+}
---
+++ src/test/scala/software/uncharted/xdata/sparkpipe/SchemaTest.scala
@@ -0,0 +1,100 @@
+/**
+ * Copyright (c) 2014-2015 Uncharted Software Inc. All rights reserved.
+ *
+ * Property of Uncharted(tm), formerly Oculus Info Inc.
+ * http://uncharted.software/
+ *
+ * This software is the confidential and proprietary information of
+ * Uncharted Software Inc. ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Uncharted Software Inc.
+ */
+package software.uncharted.xdata.sparkpipe
+
+import com.typesafe.config.ConfigFactory
+import org.apache.spark.sql.types.{BooleanType, ByteType, DateType, DoubleType, FloatType, IntegerType,
+  LongType, ShortType, StringType, StructField, TimestampType}
+import org.scalatest.FunSpec
+
+class SchemaTest extends FunSpec {
+  describe("SchemaTest") {
+    describe("#apply") {
+      it("should create a schema object from a Java Properties instance") {
+        val fields = """
+                       |fields {
+                       | a { type = boolean, index = 0}
+                       | b { type = byte, index = 1}
+                       | c { type = int, index = 2}
+                       | d { type = short, index = 3}
+                       | e { type = long, index = 4}
+                       | f { type = float, index = 5}
+                       | g { type = double, index = 6}
+                       | h { type = string, index = 7}
+                       | i { type = date, index = 8}
+                       | j { type = timestamp, index: 9}
+                       |}
+                     """.stripMargin
+
+        val config = ConfigFactory.parseString(fields)
+        val schema = Schema(config).getOrElse(fail())
+
+        info(schema.treeString)
+
+        assertResult(StructField("a", BooleanType))(schema("a"))
+        assertResult(StructField("b", ByteType))(schema("b"))
+        assertResult(StructField("c", IntegerType))(schema("c"))
+        assertResult(StructField("d", ShortType))(schema("d"))
+        assertResult(StructField("e", LongType))(schema("e"))
+        assertResult(StructField("f", FloatType))(schema("f"))
+        assertResult(StructField("g", DoubleType))(schema("g"))
+        assertResult(StructField("h", StringType))(schema("h"))
+        assertResult(StructField("i", DateType))(schema("i"))
+        assertResult(StructField("j", TimestampType))(schema("j"))
+      }
+
+      it("should fill in unspecified columns with suitable defaults") {
+        val fields = """
+                       |fields {
+                       | a { type = boolean, index = 0}
+                       | b { type = byte, index = 2}
+                       | c { type = int, index = 4}
+                       |}
+                     """.stripMargin
+
+        val config = ConfigFactory.parseString(fields)
+        val schema = Schema(config).getOrElse(fail())
+        info(schema.treeString)
+
+        assertResult(StructField("a", BooleanType))(schema("a"))
+        assertResult(StructField("__unspecified_1__", StringType))(schema("__unspecified_1__"))
+        assertResult(StructField("b", ByteType))(schema("b"))
+        assertResult(StructField("__unspecified_3__", StringType))(schema("__unspecified_3__"))
+        assertResult(StructField("c", IntegerType))(schema("c"))
+      }
+
+      it("should fail on duplicate variable indices") {
+        val fields = """
+                       |fields {
+                       | a { type = boolean, index = 1}
+                       | b { type = byte, index = 1}
+                       |}
+                     """.stripMargin
+        val config = ConfigFactory.parseString(fields)
+        assertResult(None)(Schema(config))
+      }
+
+      it("should fail on unsupported variable types") {
+        val fields = "fields { a { type = blarg, index = 0} }"
+        val config = ConfigFactory.parseString(fields)
+        assertResult(None)(Schema(config))
+      }
+
+      it("should fail on variables that don't have both type and index") {
+        val fields = "fields { a { index = 0} } "
+        val config = ConfigFactory.parseString(fields)
+        assertResult(None)(Schema(config))
+      }
+    }
+  }
+}
