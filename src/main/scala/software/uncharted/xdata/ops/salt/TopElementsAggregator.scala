46fc7ddcab27dd11f2a1c49e46ce270d91c866de
Chris Bethune <cbethune@uncharted.software>
2016-06-03 17:09:55 -0400

    adds correct license header

--- src/main/scala/software/uncharted/xdata/ops/salt/TopElementsAggregator.scala
+++ src/main/scala/software/uncharted/xdata/ops/salt/TopElementsAggregator.scala
@@ -1,17 +1,14 @@
-/*
- * Copyright 2016 Uncharted Software Inc.
+/**
+ * Copyright (c) 2014-2015 Uncharted Software Inc. All rights reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Property of Uncharted(tm), formerly Oculus Info Inc.
+ * http://uncharted.software/
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * This software is the confidential and proprietary information of
+ * Uncharted Software Inc. ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Uncharted Software Inc.
  */
 
 package software.uncharted.xdata.ops.salt
