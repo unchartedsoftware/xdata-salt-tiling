efd3a041d4c0842ef073fe13b555e434e0695ce8
Chris Bethune <cbethune@uncharted.software>
2016-06-08 14:52:59 -0400

    adds a time-base cartesian projection

---
+++ src/main/scala/software/uncharted/xdata/ops/salt/CartesianTimeProjection.scala
@@ -0,0 +1,26 @@
+/**
+  * Copyright (c) 2014-2015 Uncharted Software Inc. All rights reserved.
+  *
+  * Property of Uncharted(tm), formerly Oculus Info Inc.
+  * http://uncharted.software/
+  *
+  * This software is the confidential and proprietary information of
+  * Uncharted Software Inc. ("Confidential Information"). You shall not
+  * disclose such Confidential Information and shall use it only in
+  * accordance with the terms of the license agreement you entered into
+  * with Uncharted Software Inc.
+  */
+package software.uncharted.xdata.ops.salt
+
+import software.uncharted.salt.core.projection.numeric.CartesianProjection
+
+class CartesianTimeProjection(zoomLevels: Seq[Int],
+                              min: (Double, Double, Long) = (0.0, 0.0, 0),
+                              max: (Double, Double, Long) = (0.0, 0.0, Long.MaxValue),
+                              rangeBuckets: Long = Long.MaxValue)
+  extends XYTimeProjection(min, max, rangeBuckets, new CartesianProjection(zoomLevels, (min._1, min._2), (max._1, max._2))) {
+
+  def this(zoomLevels: Seq[Int], min: (Double, Double), max: (Double, Double), timeRange: RangeDescription[Long]) = {
+    this(zoomLevels, (min._1, min._2, timeRange.min), (max._1, max._2, timeRange.max), timeRange.count)
+  }
+}
---
+++ src/test/scala/software/uncharted/xdata/ops/salt/CartesianTimeProjectionTest.scala
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2014-2015 Uncharted Software Inc. All rights reserved.
+ *
+ * Property of Uncharted(tm), formerly Oculus Info Inc.
+ * http://uncharted.software/
+ *
+ * This software is the confidential and proprietary information of
+ * Uncharted Software Inc. ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Uncharted Software Inc.
+ */
+package software.uncharted.xdata.ops.salt
+
+import org.scalatest.FunSpec
+
+// scalastyle:off magic.number
+class CartesianTimeProjectionTest extends FunSpec {
+  describe("CartesianTimeProjectionTest") {
+    describe("#project()") {
+      it("should return None when the data space coordinate is none") {
+        val proj = new CartesianTimeProjection(Seq(0))
+        assertResult(None)(proj.project(None, (32, 32, 32)))
+      }
+
+      it("should return None when the time coord is out of range") {
+        val proj = new CartesianTimeProjection(Seq(0), (0.0, 0.0), (1.0, 1.0), RangeDescription.fromCount(0L, 100L, 10))
+        assertResult(None)(proj.project(Some(0.0, 0.0, 101L), (32, 32, 32)))
+      }
+
+      it("should return None when the coords are out of range") {
+        val proj = new CartesianTimeProjection(Seq(0), (0.0, 0.0), (1.0, 1.0), RangeDescription.fromCount(0L, 100L, 10))
+        assertResult(None)(proj.project(Some(0.0, -1.0, 10L), (32, 32, 32)))
+      }
+
+      it("should assign values to the correct time bucket") {
+        val proj = new CartesianTimeProjection(Seq(0), (0.0, 0.0), (200.0, 200.0), RangeDescription.fromCount(10L, 210L, 10))
+        assertResult(Some(List(((0, 0, 0), (0, 100, 2)))))(proj.project(Some(0.0, 0.0, 53L), (100, 100, 10)))
+      }
+    }
+
+    describe("#binTo1D()") {
+      it("should convert a (lon,lat,time) tuple into a linear coordinate") {
+        val proj = new CartesianTimeProjection(Seq(0))
+        assertResult(8064)(proj.binTo1D((4, 8, 12), (30, 20, 10)))
+      }
+    }
+  }
+}
