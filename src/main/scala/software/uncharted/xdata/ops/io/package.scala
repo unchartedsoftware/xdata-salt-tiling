e0daf60216a23b9c11921c4d4ade9f5479ea1e46
Nathan Kronenfeld <nkronenfeld@uncharted.software>
2016-09-01 13:11:40 -0400

    Separate out double-bin-tyle conversion to byte array from its surrounding structure.  Move in my alternate version of that from xdata-graph.  Test that they have the same results, and see which is faster (hint: the new one is).
    Leaving the old one in pending discussion with Chris.

--- src/main/scala/software/uncharted/xdata/ops/io/package.scala
+++ src/main/scala/software/uncharted/xdata/ops/io/package.scala
@@ -12,13 +12,15 @@
  */
 package software.uncharted.xdata.ops
 
-import java.io.{BufferedOutputStream, FileOutputStream, File}
+import java.io.{BufferedOutputStream, File, FileOutputStream}
+import java.nio.{ByteBuffer, ByteOrder, DoubleBuffer}
 import java.util.ArrayList
 import java.util.zip.ZipOutputStream
 
 import grizzled.slf4j.Logging
 import org.apache.spark.rdd.RDD
 import software.uncharted.salt.core.generation.output.SeriesData
+import software.uncharted.salt.core.util.SparseArray
 
 import scala.util.parsing.json.JSONObject
 
@@ -31,29 +33,29 @@ package object io extends Logging {
 
 
   /**
-   * Write binary array data to the file system.  Folder structure is
-   * baseFilePath/layerName/level/xIdx/yIdx.bin.  Indexing is TMS style
-   * with (0,0) in lower left, y increasing as it moves north.
-   *
-   * @param baseFilePath Baseline output directory - can store multiple layers.
-   * @param layerName Unique name for the layer .
-   * @param input tile coordinate / byte array tuples of tile data
-   * @return input data unchanged
-   */
+    * Write binary array data to the file system.  Folder structure is
+    * baseFilePath/layerName/level/xIdx/yIdx.bin.  Indexing is TMS style
+    * with (0,0) in lower left, y increasing as it moves north.
+    *
+    * @param baseFilePath Baseline output directory - can store multiple layers.
+    * @param layerName    Unique name for the layer .
+    * @param input        tile coordinate / byte array tuples of tile data
+    * @return input data unchanged
+    */
   def writeToFile(baseFilePath: String, layerName: String, extension: String)(input: RDD[((Int, Int, Int), Seq[Byte])]): RDD[((Int, Int, Int), Seq[Byte])] = {
-    new FileSystemClient(baseFilePath, Some(extension)).write(layerName, input.map{case (index, data) => (index, data.toArray)})
+    new FileSystemClient(baseFilePath, Some(extension)).write(layerName, input.map { case (index, data) => (index, data.toArray) })
     input
   }
 
   /**
-   * Write binary array data to the file system.  Folder structure is
-   * baseFilePath/filename.ext.
-   *
-   * @param baseFilePath Baseline output directory - can store multiple layer subdirectories.
-   * @param layerName Unique name for the layer.
-   * @param fileName Filename to write byte data into.
-   * @param bytes byte array of data
-   */
+    * Write binary array data to the file system.  Folder structure is
+    * baseFilePath/filename.ext.
+    *
+    * @param baseFilePath Baseline output directory - can store multiple layer subdirectories.
+    * @param layerName    Unique name for the layer.
+    * @param fileName     Filename to write byte data into.
+    * @param bytes        byte array of data
+    */
   def writeBytesToFile(baseFilePath: String, layerName: String)(fileName: String, bytes: Seq[Byte]): Unit = {
     new FileSystemClient(baseFilePath, None).writeRaw(layerName, fileName, bytes.toArray)
   }
@@ -63,26 +65,27 @@ package object io extends Logging {
     * is TMS style, with (0, 0) in the lower left, y increasing as it moves north.
     *
     * @param baseZipDirectory Baseline output directory - can store multiple layers
-    * @param layerName Unique name for this layer.  Data will go into the layer file layerName.zip
-    * @param input tile coordinate / byte array tuples of tile data
+    * @param layerName        Unique name for this layer.  Data will go into the layer file layerName.zip
+    * @param input            tile coordinate / byte array tuples of tile data
     * @return input data unchanged.
     */
-  def writeToZip (baseZipDirectory: String, layerName: String) (input: RDD[((Int, Int, Int), Seq[Byte])]): RDD[((Int, Int, Int), Seq[Byte])] = {
+  def writeToZip(baseZipDirectory: String, layerName: String)(input: RDD[((Int, Int, Int), Seq[Byte])]): RDD[((Int, Int, Int), Seq[Byte])] = {
     val zipDirectory = new File(baseZipDirectory)
     zipDirectory.mkdirs()
 
-    new ZipFileClient(zipDirectory).write(layerName + ".zip", input.map{case (index, data) => (index, data.toArray)})
+    new ZipFileClient(zipDirectory).write(layerName + ".zip", input.map { case (index, data) => (index, data.toArray) })
     input
   }
 
   /**
     * Write binary array data directly to a zip file.
+    *
     * @param baseZipDirectory Baseline output directory - can store multiple layer zip files
-    * @param layerName Unique name for the layer.  Associated zip file will be layerName.zip
-    * @param fileName The key by which the data will be put into the zip file.
-    * @param bytes byte array of data
+    * @param layerName        Unique name for the layer.  Associated zip file will be layerName.zip
+    * @param fileName         The key by which the data will be put into the zip file.
+    * @param bytes            byte array of data
     */
-  def writeBytesToZip (baseZipDirectory: String, layerName: String) (fileName: String, bytes: Seq[Byte]): Unit = {
+  def writeBytesToZip(baseZipDirectory: String, layerName: String)(fileName: String, bytes: Seq[Byte]): Unit = {
     val zipDirectory = new File(baseZipDirectory)
     zipDirectory.mkdirs()
     val zipStream = new ZipOutputStream(new FileOutputStream(new File(zipDirectory, s"${layerName}.zip"), true))
@@ -94,16 +97,16 @@ package object io extends Logging {
   }
 
   /**
-   * Write binary array data to Amazon S3 bucket.  Key format is layerName/level/xIdx/yIdx.bin.
-   * Indexing is TMS style with (0,0) in lower left, y increasing as it moves north
-   *
-   * @param accessKey AWS Access key
-   * @param secretKey AWS Secret key
-   * @param bucketName Name of S3 bucket to write to - will create a bucket if missing
-   * @param layerName Unique name for the layer
-   * @param input tile coordinate / byte array tuples of tile data
-   * @return input data unchanged
-   */
+    * Write binary array data to Amazon S3 bucket.  Key format is layerName/level/xIdx/yIdx.bin.
+    * Indexing is TMS style with (0,0) in lower left, y increasing as it moves north
+    *
+    * @param accessKey  AWS Access key
+    * @param secretKey  AWS Secret key
+    * @param bucketName Name of S3 bucket to write to - will create a bucket if missing
+    * @param layerName  Unique name for the layer
+    * @param input      tile coordinate / byte array tuples of tile data
+    * @return input data unchanged
+    */
   def writeToS3(accessKey: String, secretKey: String, bucketName: String, layerName: String)(input: RDD[((Int, Int, Int), Seq[Byte])]):
   RDD[((Int, Int, Int), Seq[Byte])] = {
     // Upload tiles to S3 using the supplied bucket and layer.  Use foreachPartition to avoid incurring
@@ -122,32 +125,33 @@ package object io extends Logging {
   }
 
   /**
-   * Write binary array data to Amazon S3 bucket.  Key format is layerName/filename.ext.
-   *
-   * @param accessKey AWS Access key
-   * @param secretKey AWS Secret key
-   * @param bucketName Name of S3 bucket to write to - will create a bucket if missing
-   * @param layerName Unique name for the layer
-   * @param bytes byte array of data to write
-   */
+    * Write binary array data to Amazon S3 bucket.  Key format is layerName/filename.ext.
+    *
+    * @param accessKey  AWS Access key
+    * @param secretKey  AWS Secret key
+    * @param bucketName Name of S3 bucket to write to - will create a bucket if missing
+    * @param layerName  Unique name for the layer
+    * @param bytes      byte array of data to write
+    */
   def writeBytesToS3(accessKey: String, secretKey: String, bucketName: String, layerName: String)(fileName: String, bytes: Seq[Byte]): Unit = {
     val s3Client = S3Client(accessKey, secretKey)
     s3Client.upload(bytes, bucketName, layerName + slash + fileName)
   }
+
   /**
-   *Write Tiling Data to the TileData column in an HBase Table
-   *
-   *RDD Layer Data is stored in their own table with tile info stored in a column and tile name as the rowID
-   * This Data is first mapped to a list of tuples containing required data to store into HBase table
-   * Then the list of tuples are sent to HBase connector write the RDDPairs into HBase
+    * Write Tiling Data to the TileData column in an HBase Table
     *
-    * @param configFile HBaseConfig Files used to connect to HBase
-   * @param layerName unique name for the layer, will be used as table name
-   * @param qualifierName name of column qualifier where data will be updated
-   * @param input RDD of tile data to be processed and stored into HBase
-   */
+    * RDD Layer Data is stored in their own table with tile info stored in a column and tile name as the rowID
+    * This Data is first mapped to a list of tuples containing required data to store into HBase table
+    * Then the list of tuples are sent to HBase connector write the RDDPairs into HBase
+    *
+    * @param configFile    HBaseConfig Files used to connect to HBase
+    * @param layerName     unique name for the layer, will be used as table name
+    * @param qualifierName name of column qualifier where data will be updated
+    * @param input         RDD of tile data to be processed and stored into HBase
+    */
   def writeToHBase(configFile: Seq[String], layerName: String, qualifierName: String)
-  (input: RDD[((Int, Int, Int), Seq[Byte])]): RDD[((Int, Int, Int), Seq[Byte])] = {
+                  (input: RDD[((Int, Int, Int), Seq[Byte])]): RDD[((Int, Int, Int), Seq[Byte])] = {
 
     val results = input.mapPartitions { tileDataIter =>
       tileDataIter.map { tileData =>
@@ -162,21 +166,22 @@ package object io extends Logging {
     input
   }
 
-  private def mkRowId (prefix: String, separator: String, suffix: String)(level: Int, x: Int, y: Int): String = {
+  private def mkRowId(prefix: String, separator: String, suffix: String)(level: Int, x: Int, y: Int): String = {
     val digits = math.log10(1 << level).floor.toInt + 1
     (prefix + "%02d" + separator + "%0" + digits + "d" + separator + "%0" + digits + "d" + suffix).format(level, x, y)
   }
+
   /**
-   *Write binary array data to a MetaData column in an HBase Table
-   *
-   *RDD Layer Data is stored in their own table with tile info stored in a column and tile name as the rowID
+    * Write binary array data to a MetaData column in an HBase Table
     *
-    * @param configFile HBaseConfig Files used to connect to HBase
-   * @param layerName unique name for the layer, will be used as table name
-   * @param qualifierName name of column qualifier where data will be updated
-   * @param fileName name of file that the data belongs to. Will be stored as the RowID
-   * @param bytes sequence of bytes to be stored in HBase Table
-   */
+    * RDD Layer Data is stored in their own table with tile info stored in a column and tile name as the rowID
+    *
+    * @param configFile    HBaseConfig Files used to connect to HBase
+    * @param layerName     unique name for the layer, will be used as table name
+    * @param qualifierName name of column qualifier where data will be updated
+    * @param fileName      name of file that the data belongs to. Will be stored as the RowID
+    * @param bytes         sequence of bytes to be stored in HBase Table
+    */
   def writeBytesToHBase(configFile: Seq[String], layerName: String, qualifierName: String)(fileName: String, bytes: Seq[Byte]): Unit = {
     val hBaseConnector = HBaseConnector(configFile)
     hBaseConnector.writeMetaData(tableName = layerName, rowID = (layerName + slash + fileName), data = bytes)
@@ -184,35 +189,66 @@ package object io extends Logging {
   }
 
   /**
-   * Serializes tile bins stored as a double array to tile index / byte sequence tuples.
+    * Serializes tile bins stored as a double array to tile index / byte sequence tuples.
     *
     * @param tiles The input tile set.
-   * @return Index/byte tuples.
-   */
-  def serializeBinArray(tiles: RDD[SeriesData[(Int, Int, Int), (Int, Int, Int), Double, (Double, Double)]]):
-  RDD[((Int, Int, Int), Seq[Byte])] = {
+    * @return Index/byte tuples.
+    */
+  def serializeBinArray[TC, BC, X](tiles: RDD[SeriesData[TC, BC, Double, X]]):
+  RDD[(TC, Seq[Byte])] = {
     tiles.filter(tile => tile.bins.density > 0)
       .map { tile =>
-        val data = for (bin <- tile.bins.seq; i <- 0 until doubleBytes) yield {
-          val datum = java.lang.Double.doubleToLongBits(bin)
-          ((datum >> (i * doubleBytes)) & 0xff).asInstanceOf[Byte]
-        }
-        (tile.coords, data.toSeq)
+        (tile.coords, doubleTileToByteArrayDense2(tile.bins))
       }
   }
 
+  // Serialize a single tile's data
+  val doubleTileToByteArrayDense1: SparseArray[Double] => Seq[Byte] = sparseData => {
+    for (bin <- sparseData.seq;  i <- 0 until doubleBytes) yield {
+      val datum = java.lang.Double.doubleToLongBits(bin);
+      ((datum >> (i * doubleBytes)) & 0xff).asInstanceOf[Byte];
+    }
+  }
+
+  // Serialize a single tile's data - an alternate version that does the same thing in a tenth the time
+  // See unit test in PackageTest for confirmation
+  val doubleTileToByteArrayDense2: SparseArray[Double] => Seq[Byte] = sparseData => {
+    val data = sparseData.seq.toArray
+    val byteBuffer = ByteBuffer.allocate(data.length * doubleBytes).order(ByteOrder.LITTLE_ENDIAN)
+    byteBuffer.asDoubleBuffer().put(DoubleBuffer.wrap(data))
+    byteBuffer.array().toSeq
+  }
+
   /**
-   * Serializes tile bins stored as a double array to tile index / byte sequence tuples.
+    * Serializes tile bins stored as a list of scored strings to tile index / byte sequence tuples.
     *
     * @param tiles The input tile set.
-   * @return Index/byte tuples.
-   */
-  def serializeElementScore(tiles: RDD[SeriesData[(Int, Int, Int), (Int, Int, Int), List[(String, Int)], Nothing]]):
-  RDD[((Int, Int, Int), Seq[Byte])] = {
+    * @return Index/byte tuples.
+    */
+  def serializeElementScore[TC, BC, X](tiles: RDD[SeriesData[TC, BC, List[(String, Int)], X]]):
+  RDD[(TC, Seq[Byte])] = {
     tiles.filter(t => t.bins.density() > 0)
       .map { t =>
         val bytes = new JSONObject(t.bins.head.toMap).toString().getBytes
         (t.coords, bytes.toSeq)
+      }
+  }
+
+  /**
+    * Serializes tile bins according to an arbitrarily specified serialization function
+    *
+    * @param serializationFcn The serialization function by which a single tile is serialized
+    * @param tiles The input tile set
+    * @tparam TC The tile coordinate type
+    * @tparam BC The bin coordinate type
+    * @tparam V The value type
+    * @tparam X Any associated analytic type
+    * @return Index/byte-array tuples
+    */
+  def serializeTiles[TC, BC, V, X](serializationFcn: SparseArray[V] => Seq[Byte])(tiles: RDD[SeriesData[TC, BC, V, X]]): RDD[(TC, Seq[Byte])] = {
+    tiles.filter(t => t.bins.density() > 0)
+      .map { tile =>
+        (tile.coords, serializationFcn(tile.bins))
       }
   }
 }
---
+++ src/test/scala/software/uncharted/xdata/ops/util/BasicOperationsTest.scala
@@ -0,0 +1,8 @@
+package software.uncharted.xdata.ops.util
+
+/**
+  * Created by nkronenfeld on 01/09/16.
+  */
+class BasicOperationsTest {
+
+}
