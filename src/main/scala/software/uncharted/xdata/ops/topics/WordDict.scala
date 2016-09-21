b24a0d146a9387c89ddb531e2b1834bba86621a4
Lyon Lay <llay@uncharted.software>
2016-09-21 11:35:48 -0400

    Refactor WordDict
    Former-commit-id: 64bb82cba10bf572dd013453be9469e339da785c

--- src/main/scala/software/uncharted/xdata/ops/topics/WordDict.scala
+++ src/main/scala/software/uncharted/xdata/ops/topics/WordDict.scala
@@ -16,13 +16,19 @@ package software.uncharted.xdata.ops.topics
 import scala.io.Source
 import org.apache.spark.rdd.RDD
 
-
+/**
+  * A library of functions to manipulate text files
+  */
 object WordDict extends Serializable {
-  def createDictFromArray(words: Array[String]) = {
-    Array.tabulate(words.length)(i => (words(i) -> i) ).toMap
+
+  def createDictFromArray(words: Array[String]) : Map[String, Int] = {
+    Array.tabulate(words.length)(i => words(i) -> i).toMap
   }
 
-  def wordcount2WordDict(wordcount: Array[(String, Int)], stopwords: Set[String], min_count: Int = 1) = {
+  def wordcount2WordDict(
+    wordcount: Array[(String, Int)],
+    stopwords: Set[String], min_count: Int = 1
+  ) : (Map[String, Int], Array[String])= {
     val filtered = wordcount.filter{case (word, count) => word.length > 2 }              // exclude words with 1-2 characters
                             .filter{case (word, count) => count >  min_count }            // exclude words with counts under min_count
                             .filterNot{case (word, count) => "^[#0-9]+$".r.findFirstIn(word).isDefined }   // exclude words made up of only digits
@@ -34,54 +40,56 @@ object WordDict extends Serializable {
     (word_dict, words)
   }
 
-  def computeWordCount(rdd: RDD[String], minCount: Int = 5) = {
+  def computeWordCount(
+    rdd: RDD[String],
+    minCount: Int = 5
+  ) : Array[(String,Int)]= {
     // extract text only, clean text of punctuation & lowercase
     val cleanRdd = rdd.map(x => TextUtil.cleanText(x) ).cache
     // find out which words we REALLY need in the word_dict (to make it smaller)
-    val wc = cleanRdd.map(x => x.split("\\s+")).flatMap(x => x).map(x => (x, 1)).reduceByKey(_ + _).collect.filter(x => x._2 > minCount)
-    wc
+    cleanRdd.map(x => x.split("\\s+")).flatMap(x => x).map(x => (x, 1)).reduceByKey(_ + _).collect.filter(x => x._2 > minCount)
   }
 
-  def computeWordCountLocal(arr: Array[String], minCount: Int = 5) = {
+  def computeWordCountLocal(
+    arr: Array[String],
+    minCount: Int = 5
+  ) : Array[(String,Int)] = {
     // extract text only, clean text of punctuation & lowercase
     val cleaned = arr.map(x => TextUtil.cleanText(x) )
     // find out which words we REALLY need in the word_dict (to make it smaller)
     val tokens = cleaned.map(x => x.split("\\s+")).flatMap(x => x)
-    val wc = tokens.groupBy(_.toString).map(x => (x._1, x._2.size)).filter(x => x._2 > minCount).toArray
-    wc
+    tokens.groupBy(_.toString).map(x => (x._1, x._2.size)).filter(x => x._2 > minCount).toArray
   }
 
-//  Deprecated due to NullPointerException
-//  def loadStopwords(swfiles: List[String]) = {
-//    def loadFile(fname: String) = {
-//      val stream: InputStream = getClass.getResourceAsStream(fname)
-//      val lines = scala.io.Source.fromInputStream(stream).getLines
-//      lines.toArray
-//    }
-//    val stopwords = swfiles.map (path => loadFile(path)).flatMap( w => w).toSet
-//    stopwords
-//  }
-
-  def loadStopwords(swfiles: List[String]) = {
-    swfiles.map(path => Source.fromFile(path).getLines.toArray).flatten.toSet
+  /**
+    * Read in the stopwords
+    *
+    * @param swfiles The list of stopword files
+    * @return A set of all stopwords
+    */
+  def loadStopwords(swfiles: List[String]) : Set[String] = {
+    swfiles.flatMap(path => Source.fromFile(path).getLines).toSet
   }
 
-  def createWordDict(rdd: RDD[String], stopwords: Set[String], minCount: Int) = {
+  def createWordDict(
+    rdd: RDD[String],
+    stopwords: Set[String],
+    minCount: Int
+  ) : (Map[String, Int], Array[String]) = {
     val wc = computeWordCount(rdd, minCount)
     println(s"wc contains ${wc.size} words")
     println("computing word dictionary...")
-    val (word_dict, words) = wordcount2WordDict(wc, stopwords, minCount)
-    (word_dict, words)
+    wordcount2WordDict(wc, stopwords, minCount)
   }
 
-  def createWordDictLocal(arr: Array[String], stopwords: Set[String], minCount: Int) = {
+  def createWordDictLocal(
+    arr: Array[String],
+    stopwords: Set[String],
+    minCount: Int
+  ) : (Map[String, Int], Array[String]) = {
     val wc = computeWordCountLocal(arr, minCount)
     println(s"wc contains ${wc.size} words")
     println("computing word dictionary...")
-    val (word_dict, words) = wordcount2WordDict(wc, stopwords, minCount)
-    (word_dict, words)
+    wordcount2WordDict(wc, stopwords, minCount)
   }
-
 }
-
-
