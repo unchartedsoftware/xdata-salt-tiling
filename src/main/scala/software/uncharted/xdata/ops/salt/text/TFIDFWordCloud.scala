9ecf6fa883d8decf388d046396a0b4b41a76a7fb
Nathan Kronenfeld <nkronenfeld@uncharted.software>
2016-09-28 17:37:04 -0400

    Implement a relatively simple TF/IDF tiling op, which takes as its documents entire tiles.

---
+++ src/main/scala/software/uncharted/xdata/ops/salt/text/TFIDFWordCloud.scala
@@ -0,0 +1,129 @@
+/**
+  * Copyright (c) 2014-2015 Uncharted Software Inc. All rights reserved.
+  *
+  * Property of Uncharted(tm), formerly Oculus Info Inc.
+  * http://uncharted.software/
+  *
+  * This software is the confidential and proprietary information of
+  * Uncharted Software Inc. ("Confidential Information"). You shall not
+  * disclose such Confidential Information and shall use it only in
+  * accordance with the terms of the license agreement you entered into
+  * with Uncharted Software Inc.
+  */
+package software.uncharted.xdata.ops.salt.text
+
+
+
+import scala.collection.mutable.{Map => MutableMap}
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.DataFrame
+import software.uncharted.salt.core.analytic.Aggregator
+import software.uncharted.salt.core.generation.output.SeriesData
+import software.uncharted.salt.core.generation.request.TileLevelRequest
+import software.uncharted.xdata.ops.salt.CartesianOp
+
+
+
+object TFIDFWordCloud extends CartesianOp {
+  def termFrequency(xCol: String,
+                    yCol: String,
+                    textCol: String,
+                    bounds: Option[(Double, Double, Double, Double)],
+                    zoomLevels: Seq[Int])
+                   (input: DataFrame):
+  RDD[SeriesData[(Int, Int, Int), (Int, Int), Map[String, Int], Nothing]] = {
+    // Pull out term and document frequencies for each tile
+    val request = new TileLevelRequest(zoomLevels, (tc: (Int, Int, Int)) => tc._1)
+    val binAggregator = new WordCounter
+
+    super.apply(1, xCol, yCol, textCol, bounds.getOrElse((0.0, 0.0, 1.0, 1.0)), zoomLevels, binAggregator, None)(request)(input)
+  }
+
+  /**
+    * Perform TFIDF on the output of termFrequency
+    *
+    * TODO: change the output to RDD[SeriesData[(Int, Int, Int), (Int, Int), Map[String, Double], Nothing]] once
+    * salt allows non-package-private creation of SeriesData objects.
+    *
+    * @param numWords
+    * @param input
+    * @return
+    */
+  def doTFIDF (numWords: Int)(input: RDD[SeriesData[(Int, Int, Int), (Int, Int), Map[String, Int], Nothing]]):
+  RDD[((Int, Int, Int), (Int, Int), Map[String, Double])] = {
+    // Calculate number of documents and the inverse document frequency
+    val (n, nD) = input.map{data =>
+      // One bin per tile - get its data
+      val level = data.coords._1
+      val tileData = data.bins(0)
+
+      // Doc count (by level)
+      val docCount = Array.fill(level+1)(0)
+      docCount(level) = 1
+      // Count of docs containing each term (by level)
+      val terms = Array.fill(level+1)(Map[String, Int]())
+      terms(level) = tileData.map{case (term, frequency) => (term -> 1)}
+
+      (docCount, terms)
+    }.reduce { (a, b) =>
+      val (docCountA, termsA) = a
+      val (docCountB, termsB) = b
+
+      val length = docCountA.length max docCountB.length
+
+      val docCount = (docCountA.padTo(length, 0) zip docCountB.padTo(length, 0)).map{case (ac, bc) => ac + bc}
+      val termsAA = termsA.padTo(length, Map[String, Int]())
+      val termsBB = termsB.padTo(length, Map[String, Int]())
+
+      val terms = (termsAA zip termsBB).map{case (ta, tb) =>
+        (ta.keys ++ tb.keys).map(key => (key -> (ta.getOrElse(key, 0) + tb.getOrElse(key, 0)))).toMap
+      }
+
+      (docCount, terms)
+    }
+
+    input.map{data =>
+      val tileCoordinate = data.coords
+      // One bin per tile
+      val binCoordinate = (1, 1)
+      val termFrequencies = data.bins(0)
+      val level = tileCoordinate._1
+      val N = n(level).toDouble
+      val termDocCounts = nD(level)
+
+      val termScores = termFrequencies.map { case (term, frequencyInDoc) =>
+        val tf = frequencyInDoc
+        val idf = math.log(N / termDocCounts(term))
+        term -> (tf * idf)
+      }
+
+      (tileCoordinate, binCoordinate, termScores)
+    }
+  }
+}
+
+object WordCounter {
+  val wordSeparators = "('$|^'|'[^a-zA-Z_0-9']+|[^a-zA-Z_0-9']+'|[^a-zA-Z_0-9'])+"
+}
+class WordCounter extends Aggregator[String, MutableMap[String, Int], Map[String, Int]] {
+  override def default(): MutableMap[String, Int] = MutableMap[String, Int]()
+
+  override def finish(intermediate: MutableMap[String, Int]): Map[String, Int] = intermediate.toMap
+
+  override def merge(left: MutableMap[String, Int], right: MutableMap[String, Int]): MutableMap[String, Int] = {
+    right.foreach{case (term, frequency) =>
+        left(term) = left.getOrElse(term, 0) + frequency
+    }
+    left
+  }
+
+  override def add(current: MutableMap[String, Int], next: Option[String]): MutableMap[String, Int] = {
+    next.foreach{input =>
+      input.split(WordCounter.wordSeparators).map(_.toLowerCase.trim).filter(!_.isEmpty).foreach(word =>
+        current(word) = current.getOrElse(word, 0) + 1
+      )
+    }
+    current
+  }
+}
---
+++ src/test/scala/software/uncharted/xdata/ops/salt/text/TFIDFWordCloudTest.scala
@@ -0,0 +1,100 @@
+/**
+  * Copyright (c) 2014-2015 Uncharted Software Inc. All rights reserved.
+  *
+  * Property of Uncharted(tm), formerly Oculus Info Inc.
+  * http://uncharted.software/
+  *
+  * This software is the confidential and proprietary information of
+  * Uncharted Software Inc. ("Confidential Information"). You shall not
+  * disclose such Confidential Information and shall use it only in
+  * accordance with the terms of the license agreement you entered into
+  * with Uncharted Software Inc.
+  */
+package software.uncharted.xdata.ops.salt.text
+
+import software.uncharted.xdata.spark.SparkFunSpec
+
+import scala.collection.mutable.{Map => MutableMap}
+import org.scalatest.FunSuite
+
+class TFIDFWordCloudTest extends SparkFunSpec {
+  describe("TF/IDF Word Cloud Test") {
+    it("should count words correctly") {
+      val input =
+        """Not, I'll not, Carrion Comfort, despair, not feast on thee;
+          |Not untwist - slack they may be - these last strands of man
+          |In me or, most weary, cry, 'I can no more.'  I can;
+          |Can something, hope, wish day cope, not choose not to be.
+          |But ah, but O thou terrible, why wouldst thou rude on me
+          |Thy wring-world right foot rock? lay a lionlimb against me? scan
+          |With darksome devouring eyes my bruised bones? and fan,
+          |O in turns of tempest, me heaped there; me frantic to avoid thee and flee?
+          |
+          |Why? That my chaff my fly; my grain lie, sheer and clear;
+          |Nay in all that toil, that coil, since (seems) I kissed the rod,
+          |Hand rather, my heart lo! lapped strength, stole joy, would laught, cheer.
+          |Cheer whom though? the hero whose heaven-handling flung me, foot trod
+          |Me? or me that fought him? O which one? is it each one? That night, that year
+          |Of now done darkness I wretch lay wrestling with (my God!) my God.""".stripMargin
+
+      val wc = new WordCounter
+      val result = wc.add(MutableMap[String, Int](), Some(input))
+      result.map{case (word, frequency) =>
+        assert(!word.contains(" "))
+        assert(!word.contains(","))
+        assert(!word.contains(";"))
+        assert(!word.contains("-"))
+        assert(!word.contains("\""))
+        assert(!word.contains("?"))
+        assert(!word.startsWith("'"))
+        assert(!word.endsWith("'"))
+      }
+      assert(8 === result("me"))
+      assert(7 === result("my"))
+      assert(1 === result("carrion"))
+      assert(1 === result("comfort"))
+    }
+
+    it("should calculate tf/idf correctly") {
+      val rddData = sc.parallelize(Seq(
+        TFIDFData(1.0, 1.0, "aaa bbb fff"),
+        TFIDFData(1.0, 3.0, "aaa bbb ggg"),
+        TFIDFData(3.0, 1.0, "aaa bbb hhh"),
+        TFIDFData(3.0, 3.0, "aaa bbb iii"),
+
+        TFIDFData(5.0, 1.0, "aaa ccc fff"),
+        TFIDFData(5.0, 3.0, "aaa ccc ggg"),
+        TFIDFData(7.0, 1.0, "aaa ccc hhh"),
+        TFIDFData(7.0, 3.0, "aaa ccc iii"),
+
+        TFIDFData(1.0, 5.0, "aaa ddd fff"),
+        TFIDFData(1.0, 7.0, "aaa ddd ggg"),
+        TFIDFData(3.0, 5.0, "aaa ddd hhh"),
+        TFIDFData(3.0, 7.0, "aaa ddd iii"),
+
+        TFIDFData(5.0, 5.0, "aaa eee fff"),
+        TFIDFData(5.0, 7.0, "aaa eee ggg"),
+        TFIDFData(7.0, 5.0, "aaa eee hhh"),
+        TFIDFData(7.0, 7.0, "aaa eee iii")
+      ))
+      val data = sqlc.createDataFrame(rddData)
+
+      val termFrequencies = TFIDFWordCloud.termFrequency("x", "y", "text", Some((0.0, 0.0, 8.0, 8.0)), Seq(0, 1, 2))(data)
+
+      val tfidf = TFIDFWordCloud.doTFIDF(5)(termFrequencies).collect.sortBy(r => 16*r._1._1 + 4*r._1._2 + r._1._3).map { r =>
+        (r._1, r._3)
+      }.toMap
+      assert(tfidf((0, 0, 0)).map(_._2).reduce(_ + _) === 0.0)
+      assert(tfidf((1, 0, 0))("aaa") === 0.0)
+      assert(tfidf((1, 1, 0))("aaa") === 0.0)
+      assert(tfidf((1, 0, 2))("aaa") === 0.0)
+      assert(tfidf((1, 1, 1))("aaa") === 0.0)
+      assert(tfidf((1, 0, 0))("bbb") === tfidf((1, 0, 0)).map(_._2).max)
+      assert(tfidf((1, 1, 0))("ccc") === tfidf((1, 0, 0)).map(_._2).max)
+      assert(tfidf((1, 0, 1))("ddd") === tfidf((1, 0, 0)).map(_._2).max)
+      assert(tfidf((1, 1, 1))("eee") === tfidf((1, 0, 0)).map(_._2).max)
+    }
+  }
+}
+
+case class TFIDFData (x: Double, y: Double, text: String)
