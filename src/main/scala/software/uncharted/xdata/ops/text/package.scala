e318f8f1203d1a5e3785156cdcd9a78bdf822589
Philippe Horne <phorne@uncharted.software>
2016-09-29 19:40:48 -0400

    Added TFIDF op that takes a DataFrame and outputs a DataFrame with the same content and an additional value containing a sequence of terms + tfidf.

---
+++ src/main/scala/software/uncharted/xdata/ops/text/package.scala
@@ -0,0 +1,58 @@
+/**
+  * Copyright (c) 2014-2015 Uncharted Software Inc. All rights reserved.
+  *
+  * Property of Uncharted(tm), formerly Oculus Info Inc.
+  * http://uncharted.software/
+  *
+  * This software is the confidential and proprietary information of
+  * Uncharted Software Inc. ("Confidential Information"). You shall not
+  * disclose such Confidential Information and shall use it only in
+  * accordance with the terms of the license agreement you entered into
+  * with Uncharted Software Inc.
+  */
+
+package software.uncharted.xdata.ops.text
+
+import org.apache.spark.sql.types.{StructType, ArrayType, StructField, StringType, DoubleType}
+import org.apache.spark.sql.{DataFrame, Row}
+import org.apache.spark.mllib.feature.{HashingTF, IDF}
+
+package object text {
+  def tfidf (inputColumnName: String, outputColumnName: String)(input: DataFrame): DataFrame = {
+
+    //Get the TF. MLLib needs an RDD of iterable.
+    val hashingTF = new HashingTF()
+    val documents = input.select(inputColumnName).rdd.map(row => row(0).asInstanceOf[Seq[String]])
+
+    val tf = hashingTF.transform(documents)
+
+    //Calculate the IDF & TFIDF.
+    val idf = new IDF().fit(tf)
+    val tfidf = idf.transform(tf)
+
+    //Create the term -> tfidf mapping.
+    val combined = documents.zip(tfidf)
+    val termTFIDF = combined.map(x => x._1.distinct.map(t => (t, x._2(hashingTF.indexOf(t)))))
+
+    //Build the combined data frame.
+    //ML would work seamlessly, but because MLLib operates on RDDs, need to push everything into a new DataFrame.
+    val rows = input.rdd.zip(termTFIDF).map{case (rowLeft, rowRight) => Row.fromSeq(rowLeft.toSeq ++ Array(rowRight))}
+    val schema = StructType(input.schema.fields :+
+      StructField(
+          outputColumnName,
+          ArrayType(
+            StructType(
+              Seq(
+                StructField("term", StringType),
+                StructField("tfidf", DoubleType, false)
+              )
+            )
+          )
+      ))
+
+    //Schema is actually input schema + Seq[Row] with Row having two columns (term, tfidf).
+    //I can't seem to get it to recognize the tuples and be input schema + Seq[(String, Double)].
+    input.sqlContext.createDataFrame(rows, schema)
+
+  }
+}
