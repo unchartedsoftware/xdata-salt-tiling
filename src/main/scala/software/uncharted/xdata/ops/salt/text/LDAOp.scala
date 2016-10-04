96f0eab23157cbd766281c3607a5e25b968c4d39
Nathan Kronenfeld <nkronenfeld@uncharted.software>
2016-10-04 18:08:48 -0400

    full lda implementation, with inadequate testing.
    Actually, oops; parts of this and my last commit are confused; that was supposed to include MatrixUtilities, this was supposed to include the LDAOperationsTests.  Last checkin won't compile as a result.

--- src/main/scala/software/uncharted/xdata/ops/salt/text/LDAOp.scala
+++ src/main/scala/software/uncharted/xdata/ops/salt/text/LDAOp.scala
@@ -12,10 +12,14 @@
   */
 package software.uncharted.xdata.ops.salt.text
 
+import org.apache.spark.SparkContext
+import software.uncharted.xdata.ops.util.BasicOperations
+import software.uncharted.xdata.spark.mllib.MatrixUtilities
+
 import scala.collection.mutable.{Map => MutableMap}
-import org.apache.spark.mllib.clustering.{DistributedLDAModel, LDA, LocalLDAModel}
+import org.apache.spark.mllib.clustering.{LDAModel, DistributedLDAModel, LDA, LocalLDAModel}
 import org.apache.spark.sql.DataFrame
-import org.apache.spark.mllib.linalg.{Matrix, SparseVector, Vector}
+import org.apache.spark.mllib.linalg.{DenseVector, Matrix, SparseVector, Vector}
 import org.apache.spark.rdd.RDD
 
 /**
@@ -54,47 +58,100 @@ object LDAOp {
 
   /**
     * Perform LDA analysis on documents in a dataframe
+    *
     * @param idCol The name of a column containing a (long) id unique to each row
     * @param textCol The name of the column containing the text to analyze
     * @param k The number of topics to find
     * @param n The number of topics to record for each document
-    * @param ldaCol The name of a column into which to store the results
     * @param input The dataframe containing the data to analyze
+    * @return The topics for each document
     */
-  def LDA (idCol: String, textCol: String, k: Int, n: Int, ldaCol: String)(input: DataFrame) = {
-    val sc = input.sqlContext.sparkContext
-
-    // Figure out our dictionary
-    val (dictionary, documents) = textToWordCount(input.select(idCol, textCol).rdd.map{row =>
+  def lda (idCol: String, textCol: String, k: Int, w: Int, n: Int)(input: DataFrame): DataFrame = {
+    val textRDD = input.select(idCol, textCol).rdd.map { row =>
       val id = row.getLong(0)
       val text = row.getString(1)
       (id, text)
-    })
+    }
 
-    val ldaModel =
-      new LDA()
-        .setK(k)
-        .setOptimizer("em")
-        .run(documents)
+    // Perform our LDA analysis
+    val rawResults = lda(k, w, n)(textRDD).map{case (index, text, scores) =>
+      // Get rid of the text, and put the results in a product we can turn to a DataFrame easily
+      DocumentTopics(index, scores)
+    }
 
-    val distLDAModel =
-      ldaModel match {
-        case distrModel: DistributedLDAModel => distrModel
-        case localModel: LocalLDAModel => {
-          localModel.save(sc, tmpDir + "lda")
-          DistributedLDAModel.load(sc, tmpDir + "lda")
-        }
-      }
+    // Reformat to a dataframe
+    BasicOperations.toDataFrame(input.sqlContext)(rawResults)
+  }
 
-    val topics: Matrix = distLDAModel.topicsMatrix
+  /**
+    * Perform LDA on an RDD of indexed documents.
+ *
+    * @param numTopics The number of topics to find
+    * @param wordsPerTopic The number of words to keep per topic
+    * @param topicsPerDocument The number of topics to record for each document
+    * @param input An RDD of indexed documents; the Long id field should be unique for each row.
+    * @return An RDD of the same documents, with a sequence of topics attached.  The third, attached, entry in each
+    *         row should be read as Seq[(topic, topicScoreForDocument)], where the topic is
+    *         Seq[(word, wordScoreForTopic)]
+    */
+  def lda (numTopics: Int, wordsPerTopic: Int, topicsPerDocument: Int)
+          (input: RDD[(Long, String)]): RDD[(Long, String, Seq[TopicScore])] = {
+    val sc = input.context
+
+    // Figure out our dictionary
+    val (dictionary, documents) = textToWordCount(input)
+
+    val model = getDistributedModel(sc, new LDA().setK(numTopics).setOptimizer("em").run(documents))
+
+    // Unwind the topics matrix using our dictionary (but reversed)
+    val allTopics = getTopics(model, dictionary, wordsPerTopic)
+
     // Doc id, topics, weights
-    val results:RDD[(Long, Array[Int], Array[Double])] = distLDAModel.topTopicsPerDocument(n)
+    val topicsByDocument: RDD[(Long, Array[Int], Array[Double])] = model.topTopicsPerDocument(topicsPerDocument)
 
-    // TODO:
-    //  (1) Unwind the topics matrix using the dictionary
-    //  (2) Unwind the two arrays above using the unwound topics matrix
-    //  (3) Make an addColumn operation (copy from aperture tiles)
-    //  (4) Use the addColumn operation to add the topic info to the dataframe, and return the result.
-    null
+    // Unwind the topics for each document
+    input.join(topicsByDocument.map { case (docId, topics, weights) =>
+      // Get the top n topic indices
+      val topTopics = topics.zip(weights).sortBy(-_._2).take(topicsPerDocument).toSeq
+
+      // Expand these into their topic word vectors
+      (docId, topTopics.map { case (index, score) =>
+        TopicScore(allTopics(index), score)
+      })
+    }).map{case (id, (document, topics)) =>
+      (id, document, topics)
+    }
+  }
+
+  private def getDistributedModel (sc: SparkContext, model: LDAModel): DistributedLDAModel = {
+    model match {
+      case distrModel: DistributedLDAModel => distrModel
+      case localModel: LocalLDAModel => {
+        localModel.save(sc, tmpDir + "lda")
+        DistributedLDAModel.load(sc, tmpDir + "lda")
+      }
+    }
+  }
+
+  private def getTopics (model: DistributedLDAModel, dictionary: Map[String, Int], wordsPerTopic: Int): Map[Int, Seq[WordScore]] = {
+    val topics: Matrix = model.topicsMatrix
+    val reverseDictionary = dictionary.map(_.swap)
+
+    (0 until topics.numCols).map(c => (c, MatrixUtilities.column(topics, c))).map { case (topicIndex, topicVector) =>
+      val wordScores = (topicVector match {
+        case v: DenseVector =>
+          v.values.zipWithIndex.map { case (value, index) =>
+            WordScore(reverseDictionary(index), value)
+          }
+        case v: SparseVector =>
+          v.indices.map(reverseDictionary(_)).zip(v.values).map{case (word, score) => WordScore(word, score)}
+      }).sortBy(-_.score).take(wordsPerTopic).toSeq
+
+      (topicIndex, wordScores)
+    }.toMap
   }
 }
+
+case class WordScore (word: String, score: Double)
+case class TopicScore (topic: Seq[WordScore], score: Double)
+case class DocumentTopics (documentIndex: Long, topics: Seq[TopicScore])
