ae5dc7c1bd2b0512f7524e30339a452d0891f0a2
AzadMemon <amemon@uncharted.software>
2016-10-11 16:54:58 -0400

    Refactored GaussianBlurSpreadingFunction to have a 2D and a 3D implementation that has the same parent.

--- src/main/scala/software/uncharted/xdata/ops/salt/GaussianBlurSpreadingFunction.scala
+++ src/main/scala/software/uncharted/xdata/ops/salt/GaussianBlurSpreadingFunction.scala
@@ -14,15 +14,13 @@
 package software.uncharted.xdata.ops.salt
 
 import software.uncharted.salt.core.spreading.SpreadingFunction
-import software.uncharted.xdata.geometry.CartesianBinning
 import software.uncharted.xdata.ops.salt.GaussianBlurSpreadingFunction.{Bin2DCoord, Bin3DCoord, TileCoord}
 
-// TODO: What was the purpose of extending CartesianBinning?
-class GaussianBlurSpreadingFunction(radius: Int, sigma: Double, maxBins: Bin2DCoord)
-  extends SpreadingFunction[TileCoord, Bin2DCoord, Double] {
+abstract class GaussianBlurSpreadingFunction[BC](radius: Int, sigma: Double)
+  extends SpreadingFunction[TileCoord, BC, Double] {
 
-  private val kernel = GaussianBlurSpreadingFunction.makeGaussianKernel(radius, sigma)
-  private val kernelDimension = GaussianBlurSpreadingFunction.calcKernelDimension(radius)
+  val kernel = GaussianBlurSpreadingFunction.makeGaussianKernel(radius, sigma)
+  val kernelDimension = GaussianBlurSpreadingFunction.calcKernelDimension(radius)
 
   /**
     * Spread a single value over multiple visualization-space coordinates
@@ -31,10 +29,7 @@ class GaussianBlurSpreadingFunction(radius: Int, sigma: Double, maxBins: Bin2DCo
     * @param value             the value to spread
     * @return Seq[(TC, BC, Option[T])] A sequence of tile coordinates, with the spread values
     */
-  // TODO: Are there types for tile coordinate, bin coordinate?
-  // TODO: Should those types take into acct 3 dimensional bin coordinates which are present in
-  // TODO: Confirm that the default value is 1.0
-  override def spread(coordsTraversable: Traversable[(TileCoord, Bin2DCoord)], value: Option[Double]): Traversable[(TileCoord, Bin2DCoord, Option[Double])] = {
+  def spread(coordsTraversable: Traversable[(TileCoord, BC)], value: Option[Double]): Traversable[(TileCoord, BC, Option[Double])] = {
     val coordsValueMap = coordsTraversable
       .flatMap(addNeighbouringBins(value.getOrElse(1.0))) // Add bins in neighborhood, affected by gaussian blur
       .groupBy(coordsValueMap => coordsValueMap._1) // Group by key. Key: (tileCoordinate, BinCoordinate2D)
@@ -45,15 +40,15 @@ class GaussianBlurSpreadingFunction(radius: Int, sigma: Double, maxBins: Bin2DCo
   }
 
   def addNeighbouringBins(value: Double)
-                         (coords: (TileCoord, Bin2DCoord)): Map[(TileCoord, Bin2DCoord), Double] = {
+                         (coords: (TileCoord, BC)): Map[(TileCoord, BC), Double] = {
     val tileCoordinate = coords._1
-    val BinCoordinate2D = coords._2
-    var result = Map(((tileCoordinate, BinCoordinate2D) -> value))
+    val binCoordinate = coords._2
+    var result = Map(((tileCoordinate, binCoordinate) -> value))
 
-    // Translate kernel coordinates into tile and bin coordinates
+    // Translate kernel coordinates into tile and bin coordinates and add them to result
     for (kernelX <- 0 to kernelDimension - 1) {
       for (kernelY <- 0 to kernelDimension - 1) {
-        val (kernelTileCoord, kernelBinCoord) = calcKernelCoord(tileCoordinate, BinCoordinate2D, (kernelX, kernelY))
+        val (kernelTileCoord, kernelBinCoord) = calcKernelCoord(tileCoordinate, binCoordinate, (kernelX, kernelY))
         if (!result.contains(kernelTileCoord, kernelBinCoord)) {
           result = result + ((kernelTileCoord, kernelBinCoord) -> 0.0)
         }
@@ -63,11 +58,10 @@ class GaussianBlurSpreadingFunction(radius: Int, sigma: Double, maxBins: Bin2DCo
     result
   }
 
-  // TODO: Can I think of a better name for coordsValueMap (the iterable) and coordsValue (the tuple?)
-  def applyKernel(coordsValueMap: Map[(TileCoord, Bin2DCoord), Double])
-                 (coordsValue: ((TileCoord, Bin2DCoord), Double)): (TileCoord, Bin2DCoord, Option[Double]) = {
-    val coords = coordsValue._1
-    val value = coordsValue._2
+  def applyKernel(coordsValueMapTraversable: Map[(TileCoord, BC), Double])
+                 (coordsValueMap: ((TileCoord, BC), Double)): (TileCoord, BC, Option[Double]) = {
+    val coords = coordsValueMap._1
+    val data = coordsValueMap._2
     val tileCoordinate = coords._1
     val binCoordinate = coords._2
     var result = List[Double]()
@@ -75,16 +69,33 @@ class GaussianBlurSpreadingFunction(radius: Int, sigma: Double, maxBins: Bin2DCo
     for (kernelX <- 0 to kernelDimension - 1) {
       for (kernelY <- 0 to kernelDimension - 1) {
         val (kernelTileCoord, kernelBinCoord) = calcKernelCoord(tileCoordinate, binCoordinate, (kernelX, kernelY))
-        val valueAtCoord = coordsValueMap.get((kernelTileCoord, kernelBinCoord))
-        result = result :+ kernel(kernelX)(kernelY) * valueAtCoord.getOrElse(0.0)
+        val coordValue = coordsValueMapTraversable.get((kernelTileCoord, kernelBinCoord))
+        result = result :+ kernel(kernelX)(kernelY) * coordValue.getOrElse(0.0) // If no value found, then coordinate isn't part of the original coordsTraversable, so assume value is 0.
       }
     }
 
     (tileCoordinate, binCoordinate, Some(result.sum))
   }
 
+  def calcKernelCoord(tileCoord: TileCoord, binCoordinate: BC, kernelIndex: Bin2DCoord): (TileCoord, BC)
+
+  // TODO: Do I need to take into consideration tms here?
+  def translateLeft(tileCoordinate: TileCoord) = (tileCoordinate._1 - 1, tileCoordinate._2, tileCoordinate._3)
+
+  def translateRight(tileCoordinate: TileCoord) = (tileCoordinate._1 + 1, tileCoordinate._2, tileCoordinate._3)
+
+  def translateUp(tileCoordinate: TileCoord) = (tileCoordinate._1, tileCoordinate._2 + 1, tileCoordinate._3)
+
+  def translateDown(tileCoordinate: TileCoord) = (tileCoordinate._1, tileCoordinate._2 - 1, tileCoordinate._3)
+}
+
+class GaussianBlurSpreadingFunction2D(radius: Int, sigma: Double, maxBins: Bin2DCoord)
+  extends GaussianBlurSpreadingFunction[Bin2DCoord](radius: Int, sigma: Double) {
+
   def calcKernelCoord(tileCoord: TileCoord, binCoord: Bin2DCoord, kernelIndex: Bin2DCoord): (TileCoord, Bin2DCoord) = {
-    var kernelBinCoord = (binCoord._1 + kernelIndex._1 - Math.floorDiv(kernelDimension, 2), binCoord._2 + kernelIndex._2 - Math.floorDiv(kernelDimension, 2))
+    var kernelXCoord = binCoord._1 + kernelIndex._1 - Math.floor(kernelDimension / 2).toInt
+    var kernelYCoord = binCoord._2 + kernelIndex._2 - Math.floor(kernelDimension / 2).toInt
+    var kernelBinCoord = (kernelXCoord, kernelYCoord)
     var kernelTileCoord = tileCoord
 
     // If kernel bin coordinate lies outside of the tile, calculate new coordinates for tile and bin
@@ -106,92 +117,21 @@ class GaussianBlurSpreadingFunction(radius: Int, sigma: Double, maxBins: Bin2DCo
 
     (kernelTileCoord, kernelBinCoord)
   }
-
-  // TODO: Consider, will tms need to be taken into acct here?
-  def translateLeft(tileCoordinate: TileCoord) = (tileCoordinate._1 - 1, tileCoordinate._2, tileCoordinate._3)
-
-  def translateRight(tileCoordinate: TileCoord) = (tileCoordinate._1 + 1, tileCoordinate._2, tileCoordinate._3)
-
-  def translateUp(tileCoordinate: TileCoord) = (tileCoordinate._1, tileCoordinate._2 + 1, tileCoordinate._3)
-
-  def translateDown(tileCoordinate: TileCoord) = (tileCoordinate._1, tileCoordinate._2 - 1, tileCoordinate._3)
 
   def calcBinCoordInLeftTile(kernelBinCoord: Bin2DCoord) = (maxBins._1 + kernelBinCoord._1 + 1, kernelBinCoord._2)
 
   def calcBinCoordInRightTile(kernelBinCoord: Bin2DCoord) = (kernelBinCoord._1 - maxBins._1 - 1, kernelBinCoord._2)
 
-  def calcBinCoordInTopTile(kernelBinCoord: Bin2DCoord) = (kernelBinCoord._1, maxBins._1 + kernelBinCoord._2 + 1)
+  def calcBinCoordInTopTile(kernelBinCoord: Bin2DCoord) = (kernelBinCoord._1, maxBins._2 + kernelBinCoord._2 + 1)
 
-  def calcBinCoordInBottomTile(kernelBinCoord: Bin2DCoord) = (kernelBinCoord._1, kernelBinCoord._2 - maxBins._1 - 1)
+  def calcBinCoordInBottomTile(kernelBinCoord: Bin2DCoord) = (kernelBinCoord._1, kernelBinCoord._2 - maxBins._2 - 1)
 }
 
 class GaussianBlurSpreadingFunction3D(radius: Int, sigma: Double, maxBins: Bin3DCoord)
-  extends SpreadingFunction[TileCoord, Bin3DCoord, Double] {
+  extends GaussianBlurSpreadingFunction[Bin3DCoord](radius: Int, sigma: Double) {
 
-  private val kernel = GaussianBlurSpreadingFunction.makeGaussianKernel(radius, sigma)
-  private val kernelDimension = GaussianBlurSpreadingFunction.calcKernelDimension(radius)
-
-  /**
-    * Spread a single value over multiple visualization-space coordinates
-    *
-    * @param coordsTraversable the visualization-space coordinates
-    * @param value             the value to spread
-    * @return Seq[(TC, BC, Option[T])] A sequence of tile coordinates, with the spread values
-    */
-  // TODO: Are there types for tile coordinate, bin coordinate?
-  // TODO: Should those types take into acct 3 dimensional bin coordinates which are present in
-  // TODO: Confirm that the default value is 1.0
-  override def spread(coordsTraversable: Traversable[(TileCoord, Bin3DCoord)], value: Option[Double]): Traversable[(TileCoord, Bin3DCoord, Option[Double])] = {
-    val coordsValueMap = coordsTraversable
-      .flatMap(addNeighbouringBins(value.getOrElse(1.0))) // Add bins in neighborhood, affected by gaussian blur
-      .groupBy(coordsValueMap => coordsValueMap._1) // Group by key. Key: (tileCoordinate, BinCoordinate2D)
-      .map({ case (group, traversable) => traversable.reduce { (a, b) => (a._1, a._2 + b._2) } }) // Reduces by key, adding the values
-
-    // TODO: Can I think of a better way to write this
-    coordsValueMap.map(applyKernel(coordsValueMap))
-  }
-
-  def addNeighbouringBins(value: Double)
-                         (coords: (TileCoord, Bin3DCoord)): Map[(TileCoord, Bin3DCoord), Double] = {
-    val tileCoordinate = coords._1
-    val binCoordinate = coords._2
-    var result = Map(((tileCoordinate, binCoordinate) -> value))
-
-    // Translate kernel coordinates into tile and bin coordinates
-    for (kernelX <- 0 to kernelDimension - 1) {
-      for (kernelY <- 0 to kernelDimension - 1) {
-        val (kernelTileCoord, kernelBinCoord) = calcKernelCoord(tileCoordinate, binCoordinate, (kernelX, kernelY, binCoordinate._3))
-        if (!result.contains(kernelTileCoord, kernelBinCoord)) {
-          result = result + ((kernelTileCoord, kernelBinCoord) -> 0.0)
-        }
-      }
-    }
-
-    result
-  }
-
-  // TODO: Can I think of a better name for coordsValueMap (the iterable) and coordsValue (the tuple?)
-  def applyKernel(coordsValueMap: Map[(TileCoord, Bin3DCoord), Double])
-                 (coordsValue: ((TileCoord, Bin3DCoord), Double)): (TileCoord, Bin3DCoord, Option[Double]) = {
-    val coords = coordsValue._1
-    val value = coordsValue._2
-    val tileCoordinate = coords._1
-    val binCoordinate = coords._2
-    var result = List[Double]()
-
-    for (kernelX <- 0 to kernelDimension - 1) {
-      for (kernelY <- 0 to kernelDimension - 1) {
-        val (kernelTileCoord, kernelBinCoord) = calcKernelCoord(tileCoordinate, binCoordinate, (kernelX, kernelY, binCoordinate._3))
-        val valueAtCoord = coordsValueMap.get((kernelTileCoord, kernelBinCoord))
-        result = result :+ kernel(kernelX)(kernelY) * valueAtCoord.getOrElse(0.0)
-      }
-    }
-
-    (tileCoordinate, binCoordinate, Some(result.sum))
-  }
-
-  def calcKernelCoord(tileCoord: TileCoord, binCoord: Bin3DCoord, kernelIndex: Bin3DCoord): (TileCoord, Bin3DCoord) = {
-    var kernelBinCoord = (binCoord._1 + kernelIndex._1 - Math.floorDiv(kernelDimension, 2), binCoord._2 + kernelIndex._2 - Math.floorDiv(kernelDimension, 2), binCoord._3)
+  def calcKernelCoord(tileCoord: TileCoord, binCoord: Bin3DCoord, kernelIndex: Bin2DCoord): (TileCoord, Bin3DCoord) = {
+    var kernelBinCoord = (binCoord._1 + kernelIndex._1 - Math.floor(kernelDimension / 2).toInt, binCoord._2 + kernelIndex._2 - Math.floor(kernelDimension / 2).toInt, binCoord._3)
     var kernelTileCoord = tileCoord
 
     // If kernel bin coordinate lies outside of the tile, calculate new coordinates for tile and bin
@@ -214,22 +154,13 @@ class GaussianBlurSpreadingFunction3D(radius: Int, sigma: Double, maxBins: Bin3D
     (kernelTileCoord, kernelBinCoord)
   }
 
-  // TODO: Consider, will tms need to be taken into acct here?
-  def translateLeft(tileCoordinate: TileCoord) = (tileCoordinate._1 - 1, tileCoordinate._2, tileCoordinate._3)
-
-  def translateRight(tileCoordinate: TileCoord) = (tileCoordinate._1 + 1, tileCoordinate._2, tileCoordinate._3)
-
-  def translateUp(tileCoordinate: TileCoord) = (tileCoordinate._1, tileCoordinate._2 + 1, tileCoordinate._3)
-
-  def translateDown(tileCoordinate: TileCoord) = (tileCoordinate._1, tileCoordinate._2 - 1, tileCoordinate._3)
-
   def calcBinCoordInLeftTile(kernelBinCoord: Bin3DCoord) = (maxBins._1 + kernelBinCoord._1 + 1, kernelBinCoord._2, kernelBinCoord._3)
 
   def calcBinCoordInRightTile(kernelBinCoord: Bin3DCoord) = (kernelBinCoord._1 - maxBins._1 - 1, kernelBinCoord._2, kernelBinCoord._3)
 
-  def calcBinCoordInTopTile(kernelBinCoord: Bin3DCoord) = (kernelBinCoord._1, maxBins._1 + kernelBinCoord._2 + 1, kernelBinCoord._3)
+  def calcBinCoordInTopTile(kernelBinCoord: Bin3DCoord) = (kernelBinCoord._1, maxBins._2 + kernelBinCoord._2 + 1, kernelBinCoord._3)
 
-  def calcBinCoordInBottomTile(kernelBinCoord: Bin3DCoord) = (kernelBinCoord._1, kernelBinCoord._2 - maxBins._1 - 1, kernelBinCoord._3)
+  def calcBinCoordInBottomTile(kernelBinCoord: Bin3DCoord) = (kernelBinCoord._1, kernelBinCoord._2 - maxBins._2 - 1, kernelBinCoord._3)
 }
 
 object GaussianBlurSpreadingFunction {
@@ -242,49 +173,26 @@ object GaussianBlurSpreadingFunction {
     val kernel = Array.ofDim[Double](kernelDimension, kernelDimension)
     var sum = 0.0
 
-    for (u <- 0 until kernelDimension) {
-      for (v <- 0 until kernelDimension) {
-        val uc = u - (kernel.length - 1) / 2
-        val vc = v - (kernel(0).length - 1) / 2
+    for (x <- 0 until kernelDimension) {
+      for (y <- 0 until kernelDimension) {
+        val uc = x - (kernel.length - 1) / 2
+        val vc = y - (kernel(0).length - 1) / 2
         // Calculate and save
         val g = Math.exp(-(uc * uc + vc * vc) / (2 * sigma * sigma))
         sum += g
-        kernel(u)(v) = g
+        kernel(x)(y) = g
       }
     }
 
     // Normalize the kernel
-    for (u <- 0 until kernel.length) {
-      for (v <- 0 until kernel(0).length) {
-        kernel(u)(v) /= sum
+    for (x <- 0 until kernel.length) {
+      for (y <- 0 until kernel(0).length) {
+        kernel(x)(y) /= sum
       }
     }
+
     kernel
   }
 
   def calcKernelDimension(radius: Int) = 2 * radius + 1
-
-  // TODO: Remove this at end
-  def main(args: Array[String]): Unit = {
-    val spreadingFunction = new GaussianBlurSpreadingFunction(4, 3.0, (255, 255))
-    val kernel = makeGaussianKernel(4, 3.0)
-    val coordMap = spreadingFunction.spread(List(
-      ((1, 1, 1), (150, 151)),
-      ((1, 1, 1), (150, 152)),
-      ((1, 1, 1), (150, 153)),
-      ((1, 1, 1), (150, 154)),
-      ((1, 1, 1), (150, 155)),
-      ((1, 1, 1), (150, 156)),
-      ((1, 1, 1), (150, 157)),
-      ((1, 1, 1), (150, 158)),
-      ((1, 1, 1), (150, 159)),
-      ((1, 1, 1), (150, 160)),
-      ((1, 1, 1), (150, 161)),
-      ((1, 1, 1), (150, 162)),
-      ((1, 1, 1), (150, 163)),
-      ((1, 1, 1), (150, 164))
-    ), Some(1.0))
-
-    coordMap.toList.sortBy((coord) => (coord._2._2, coord._2._1)).foreach(println)
-  }
 }
