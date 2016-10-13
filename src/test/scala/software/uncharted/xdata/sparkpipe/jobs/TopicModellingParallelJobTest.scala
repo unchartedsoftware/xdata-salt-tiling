3ee2a12d8411d8bdb0f21bcbe8f3f008f4347618
Lyon Lay <llay@uncharted.software>
2016-10-13 15:46:18 -0400

    Created tests
    Former-commit-id: 4b9c23054f6919146093a425802591d3e6e69b4c

--- src/test/scala/software/uncharted/xdata/sparkpipe/jobs/TopicModellingParallelJobTest.scala
+++ src/test/scala/software/uncharted/xdata/sparkpipe/jobs/TopicModellingParallelJobTest.scala
@@ -12,30 +12,48 @@
  */
 package software.uncharted.xdata.sparkpipe.jobs
 
+import net.liftweb.json._
+import net.liftweb.json.JsonDSL._
+import org.apache.commons.io.FileUtils
 import org.scalatest.FunSpec
+import scala.io.Source.fromFile
 
 // scalastyle:off multiple.string.literals
 class TopicModellingJobTest extends FunSpec {
 
+  private val testOutputDir: String = "build/tmp/test_file_output/test_heatmap"
+  private val suffix: String = "bin"
+
   describe("TopicModellingParallelJobTest") {
-    describe("#main()") {
-      it("should do topic modelling and write results to file") {
-        // When test are run from another project that includes this project, the current working directory is set such
-        // that the data files referenced in tiling-file-io.conf can't be found.  To fix this we reset the CWD to the
-        // xdata-pipeline-ops directory, and reset it afterwards.
-        val oldDir = System.getProperty("user.dir")
-        try {
-          val path = classOf[TopicModellingJobTest].getResource("/topic-modelling/topic-modelling.conf").toURI.getPath
-          // Run the test from path/to/xdata-pipeline-ops/
-          val newDir = path.substring(0, path.indexOf("xdata-pipeline-ops") + 18)
-          System.setProperty("user.dir", newDir)
+    // When test are run from another project that includes this project, the current working directory is set such
+    // that the data files referenced in tiling-file-io.conf can't be found.  To fix this we reset the CWD to the
+    // xdata-pipeline-ops directory, and reset it afterwards.
+    val oldDir = System.getProperty("user.dir")
+    try {
+      val path = classOf[TopicModellingJobTest].getResource("/topic-modelling/topic-modelling.conf").toURI.getPath
+      // Run the test from path/to/xdata-pipeline-ops/
+      val newDir = path.substring(0, path.indexOf("xdata-pipeline-ops") + 18)
+      System.setProperty("user.dir", newDir)
 
+      describe("#main()") {
+
+        it("should run the topic modelling job") {
           TopicModellingJob.main(Array(path))
+        }
 
-        } finally {
-          System.setProperty("user.dir", oldDir)
+        val expected = fromFile("src/test/resources/topic-modelling/results/expected/part-00000").getLines
+        val actual = fromFile("src/test/resources/topic-modelling/results/actual/part-00000").getLines
+
+        it("should have an output that has the same number of rows as the expected output"){
+          assert(expected.length == actual.length)
+        }
+
+        it("should have output that matches the expected output exactly") {
+          assert((expected zip actual).forall(x => x._1 == x._2))
         }
       }
+    } finally {
+      System.setProperty("user.dir", oldDir)
     }
   }
 }
