2eca66e077dfe28cbe7b7c69d140c580f9ce6aa1
Chris Bethune <cbethune@uncharted.software>
2017-04-17 16:22:40 -0400

    updates license to MIT
    Former-commit-id: e9784db1dce3c51bdd65b2e6b54923c389c30680

--- src/main/scala/software/uncharted/xdata/ops/topics/twitter/package.scala
+++ src/main/scala/software/uncharted/xdata/ops/topics/twitter/package.scala
@@ -1,15 +1,31 @@
 /**
-  * Copyright (c) 2014-2015 Uncharted Software Inc. All rights reserved.
-  *
-  * Property of Uncharted(tm), formerly Oculus Info Inc.
-  * http://uncharted.software/
-  *
-  * This software is the confidential and proprietary information of
-  * Uncharted Software Inc. ("Confidential Information"). You shall not
-  * disclose such Confidential Information and shall use it only in
-  * accordance with the terms of the license agreement you entered into
-  * with Uncharted Software Inc.
-  */
+ * Copyright © 2013-2017 Uncharted Software Inc.
+ *
+ * Property of Uncharted™, formerly Oculus Info Inc.
+ *
+ * http://uncharted.software/
+ *
+ * Released under the MIT License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package software.uncharted.xdata.ops.topics
 
 import org.apache.spark.broadcast.Broadcast
